d39867898c29fcd4044238862e58a9d30f1f9a7f
kiv1n <aloha@kiv1n.ru>
2025-04-29 08:26:07 +0300

    Move favicon.ico and global.css, update name and description

---
+++ app/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Answer 2 Earn",
+  description:
+    "Stake, ask, answer, and earn on the Q&A platform powered by Lukso and AI",
+  links: {
+    github: "https://github.com/web3goals/answer-2-earn-prototype",
+    twitter: "https://twitter.com/kiv1n",
+  },
+};
