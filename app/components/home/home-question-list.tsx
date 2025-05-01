745843a87dc1c2048c61d097e20e0a0354bccbe4
kiv1n <aloha@kiv1n.ru>
2025-05-01 17:58:17 +0300

    Update layout spacing in HomeQuestionList component

--- app/components/home/home-question-list.tsx
+++ app/components/home/home-question-list.tsx
@@ -31,7 +31,7 @@ export function HomeQuestionList(props: {
   ];
 
   return (
-    <div className="flex flex-col items-center gap-8 mt-8">
+    <div className="flex flex-col items-center gap-12 mt-12">
       {groups.map((group, index) => (
         <div key={index} className="w-full flex flex-col gap-2">
           <h2 className="text-3xl font-semibold tracking-tight text-center mb-2">
