8f3f8f0294886fbe6940050059d80d0d3aa454fe
kiv1n <aloha@kiv1n.ru>
2025-05-03 09:47:01 +0300

    Add additional remote pattern for image loading in Next.js config

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://api.universalprofile.cloud/ipfs/**")],
+    remotePatterns: [
+      new URL("https://api.universalprofile.cloud/ipfs/**"),
+      new URL("https://api.universalprofile.cloud/image/**"),
+    ],
   },
 };
 
